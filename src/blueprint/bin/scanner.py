b671b174844712fdf0413dd3195a1cf7313b75b3
Giacomo 'Mr. Wolf' Furlan <giacomo.furlan@fastwebnet.it>
2022-01-02 00:54:14 +0100

    Scanner CLI: add package root argument

--- src/blueprint/bin/scanner.py
+++ src/blueprint/bin/scanner.py
@@ -1,16 +1,20 @@
-from os.path import abspath, dirname, sep
+from argparse import ArgumentParser
+from pathlib import Path
 from blueprint.module_scanner import functions_scanner
 
 
 def main():
-    print('')
-    print('')
-    print('')
-    print('')
-    path = abspath(
-        sep.join([dirname(__file__), '..', 'alperia-dp', 'glue', 'etl']))
+    args = ArgumentParser()
+    args.add_argument('package_root', help='root of the package to scan')
+    args = args.parse_args()
 
-    print(functions_scanner(path, r'_test'))
+    print('')
+    print('')
+    print('')
+    print('')
+    path = Path(args.package_root)
+
+    print(functions_scanner(path.absolute(), r'_test'))
 
 
 if __name__ == '__main__':
