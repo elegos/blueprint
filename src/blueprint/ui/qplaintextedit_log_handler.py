962e84375fda567e7d903b4092f06ea6e6de3ac7
Giacomo 'Mr. Wolf' Furlan <giacomo.furlan@fastwebnet.it>
2022-01-08 00:17:38 +0100

    Thread-safe QPlainTextEditLogHandler

--- src/blueprint/ui/qplaintextedit_log_handler.py
+++ src/blueprint/ui/qplaintextedit_log_handler.py
@@ -1,10 +1,16 @@
 import logging
+from PySide6.QtCore import QObject, Signal, SignalInstance
 
 from PySide6.QtWidgets import QPlainTextEdit
 
 
+class LogObject(QObject):
+    signal: SignalInstance = Signal(str)
+
+
 class QPlainTextEditLogHandler(logging.Handler):
     widget: QPlainTextEdit
+    log_object: LogObject
 
     def __init__(self, widget: QPlainTextEdit) -> None:
         super().__init__(level=logging.root.level)
@@ -16,7 +22,13 @@ class QPlainTextEditLogHandler(logging.Handler):
         self.widget.setReadOnly(True)
         self.widget.setCenterOnScroll(True)
 
-    def emit(self, record: logging.LogRecord) -> None:
-        message = self.format(record)
+        self.log_object = LogObject()
+        self.log_object.signal.connect(self.on_message_emitted)
+
+    def on_message_emitted(self, message: str) -> None:
         self.widget.appendPlainText(message)
         self.widget.ensureCursorVisible()
+
+    def emit(self, record: logging.LogRecord) -> None:
+        message = self.format(record)
+        self.log_object.signal.emit(message)
